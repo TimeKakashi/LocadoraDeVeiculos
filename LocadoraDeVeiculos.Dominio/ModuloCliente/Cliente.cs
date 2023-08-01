30de3244031d6a6e9af6546022b3483efc662dc6
jggomes14 <jggomes141097@gmail.com>
2023-08-01 12:45:21 -0300

    Todas as definições da Classe Cliente feitas, resta trabalhar a tela e as funcionalidades.

---
+++ LocadoraDeVeiculos.Dominio/ModuloCliente/Cliente.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LocadoraDeVeiculos.Dominio.Compartilhado;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloCliente
+{
+    public class Cliente : EntidadeBase<Cliente>
+    {
+        public enum TipoCliente
+        {
+            PessoaFisica,
+            PessoaJuridica
+        }
+
+        public TipoCliente Tipo { get; private set; }
+
+        // Atributos comuns a Pessoa Física e Jurídica
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public Endereco Endereco { get; set; }
+
+        // Atributos específicos para Pessoa Física
+        public string CPF { get; set; }
+        public string RG { get; set; }
+        public string CNH { get; set; }
+        public Cliente PessoaJuridicaAssociada { get; set; } // Referência à Pessoa Jurídica associada, pode ser nulo
+
+        // Atributos específicos para Pessoa Jurídica
+        public string CNPJ { get; set; }
+        public string CondutorCPF { get; set; }
+
+        public Cliente(string nome, string telefone, Endereco endereco, string cpf, string rg, string cnh)
+        {
+            Tipo = TipoCliente.PessoaFisica;
+            Nome = nome;
+            Telefone = telefone;
+            Endereco = endereco;
+            CPF = cpf;
+            RG = rg;
+            CNH = cnh;
+        }
+
+        public Cliente(string nome, string telefone, Endereco endereco, string cnpj, string condutorCpf)
+        {
+            Tipo = TipoCliente.PessoaJuridica;
+            Nome = nome;
+            Telefone = telefone;
+            Endereco = endereco;
+            CNPJ = cnpj;
+            CondutorCPF = condutorCpf;
+        }
+
+        public override void Atualizar(Cliente registro)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Endereco
+    {
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string CEP { get; set; }
+    }
+}
---
+++ LocadoraDeVeiculos.Dominio/ModuloCliente/ValidadorCliente.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.ModuloCliente
+{
+    using FluentValidation;
+
+    public class ValidadorCliente : AbstractValidator<Cliente>, IValidadorCliente
+    {
+        public ValidadorCliente()
+        {
+            RuleFor(c => c.Nome).NotEmpty().NotNull().MinimumLength(4);
+            RuleFor(c => c.Telefone).NotEmpty().NotNull();
+            RuleFor(c => c.Endereco).SetValidator(new ValidadorEndereco());
+
+            When(c => c.Tipo == Cliente.TipoCliente.PessoaFisica, () =>
+            {
+                RuleFor(c => c.CPF).NotEmpty().NotNull().Length(11);
+                RuleFor(c => c.RG).NotEmpty().NotNull();
+                RuleFor(c => c.CNH).NotEmpty().NotNull();
+            });
+
+            When(c => c.Tipo == Cliente.TipoCliente.PessoaJuridica, () =>
+            {
+                RuleFor(c => c.CNPJ).NotEmpty().NotNull().Length(14);
+                RuleFor(c => c.CondutorCPF).NotEmpty().NotNull().Length(11);
+            });
+        }
+    }
+
+    public class ValidadorEndereco : AbstractValidator<Endereco>
+    {
+        public ValidadorEndereco()
+        {
+            RuleFor(e => e.Logradouro).NotEmpty().NotNull();
+            RuleFor(e => e.Numero).NotEmpty().NotNull();
+            RuleFor(e => e.Bairro).NotEmpty().NotNull();
+            RuleFor(e => e.Cidade).NotEmpty().NotNull();
+            RuleFor(e => e.Estado).NotEmpty().NotNull();
+            RuleFor(e => e.CEP).NotEmpty().NotNull().Length(8);
+        }
+    }
+
+}
