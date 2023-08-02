9f8a374782aa2be6b29b45b515d7e31e6ba2079a
jggomes14 <jggomes141097@gmail.com>
2023-08-02 16:01:26 -0300

    correção de erros

--- LocadoraDeVeiculos/ModuloCliente/TabelaCliente.Designer.cs
+++ LocadoraDeVeiculos/ModuloCliente/TabelaCliente.Designer.cs
@@ -28,10 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            grid = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)grid).BeginInit();
+            SuspendLayout();
+            // 
+            // grid
+            // 
+            grid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            grid.Dock = DockStyle.Fill;
+            grid.Location = new Point(0, 0);
+            grid.Name = "grid";
+            grid.RowTemplate.Height = 25;
+            grid.Size = new Size(596, 523);
+            grid.TabIndex = 0;
+            // 
+            // TabelaCliente
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(grid);
+            Name = "TabelaCliente";
+            Size = new Size(596, 523);
+            ((System.ComponentModel.ISupportInitialize)grid).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView grid;
     }
 }
